6f058aa49ab97e6e3388aaeaf24b155c58f1ccc9
Chervonnyy Anton <anton@chervonnyy.ru>
2022-02-03 21:09:05 +0100

    проект 4го спринта

---
+++ src/main/java/Account.java
@@ -0,0 +1,26 @@
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() throws NullPointerException{
+
+        try {
+        boolean isEmpty = name.isEmpty();
+        boolean isBlanc = name.isBlank();
+        boolean lengthOK = name.length() >= 3 && name.length() <= 19;
+        boolean startSpace = name.startsWith(" ");
+        boolean endSpace = name.endsWith(" ");
+        boolean singleSpace = name.indexOf(" ") == name.lastIndexOf(" ") && name.indexOf(" ") != -1;
+
+
+        return !isEmpty && !isBlanc && !endSpace && !startSpace && lengthOK && singleSpace;
+
+        } catch (NullPointerException e){
+            return false;
+        }
+    }
+}
